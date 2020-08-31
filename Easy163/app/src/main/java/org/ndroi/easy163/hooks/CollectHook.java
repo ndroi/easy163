2b3db1255d4aad611b6190b455fa06c29415c3dd
ndroi <ndroi@ndroi.ndroi>
2020-08-31 22:45:36 +0800

    支持下架歌曲收藏

--- Easy163/app/src/main/java/org/ndroi/easy163/hooks/CollectHook.java
+++ Easy163/app/src/main/java/org/ndroi/easy163/hooks/CollectHook.java
@@ -1,5 +1,7 @@
 package org.ndroi.easy163.hooks;
 
+import android.util.Log;
+
 import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 
@@ -22,11 +24,24 @@ public class CollectHook extends BaseHook
         return path.endsWith("/playlist/manipulate/tracks");
     }
 
+    public void hookRequest(Request request)
+    {
+        super.hookRequest(request);
+        Crypto.Request cryptoRequest = Crypto.decryptRequestBody(new String(request.getContent()));
+        String trackId = cryptoRequest.json.getString("trackIds");
+        trackId = trackId.substring(2, trackId.length() - 2);
+        String pid = cryptoRequest.json.getString("pid");
+        String op = cryptoRequest.json.getString("op");
+        String postData = "trackIds=[" + trackId + "," + trackId +"]&pid=" + pid + "&op=" + op;
+        request.setUri("http://music.163.com/api/playlist/manipulate/tracks");
+        request.setContent(postData.getBytes());
+    }
+
     @Override
     public void hookResponse(Response response)
     {
         super.hookResponse(response);
-        byte[] bytes = Crypto.aesDecrypt(response.getContent());
+        byte[] bytes = response.getContent();
         JSONObject jsonObject = JSONObject.parseObject(new String(bytes));
         jsonObject.put("code", 200);
         jsonObject.remove("message");
