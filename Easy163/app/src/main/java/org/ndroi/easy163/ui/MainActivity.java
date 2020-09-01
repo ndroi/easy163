53f2b51b022c948df6b306d197af4bf4e303762c
Bozhen Zhao <zhaobozhen2025@gmail.com>
2020-09-01 17:59:38 +0800

    Update UI.

--- Easy163/app/src/main/java/org/ndroi/easy163/ui/MainActivity.java
+++ Easy163/app/src/main/java/org/ndroi/easy163/ui/MainActivity.java
@@ -7,8 +7,9 @@ import android.content.IntentFilter;
 import android.net.VpnService;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.CompoundButton;
-import android.widget.ToggleButton;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -16,17 +17,19 @@ import org.ndroi.easy163.R;
 import org.ndroi.easy163.utils.EasyLog;
 import org.ndroi.easy163.vpn.LocalVPNService;
 
-public class MainActivity extends AppCompatActivity implements ToggleButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity {
 
   private static final int VPN_REQUEST_CODE = 0x0F;
-  private ToggleButton toggleButton = null;
+  private ViewGroup serviceLayout = null;
+  private boolean isRunning = false;
 
   private BroadcastReceiver serviceReceiver = new BroadcastReceiver() {
     @Override
     public void onReceive(Context context, Intent intent) {
       boolean isServiceRunning = intent.getBooleanExtra("isRunning", false);
       Log.d("MainActivity", "BroadcastReceiver service isRunning: " + isServiceRunning);
-      toggleButton.setChecked(isServiceRunning);
+      isRunning = isServiceRunning;
+      setServiceLayout(isServiceRunning);
     }
   };
 
@@ -35,24 +38,26 @@ public class MainActivity extends AppCompatActivity implements ToggleButton.OnCh
     super.onCreate(savedInstanceState);
 
     setContentView(R.layout.activity_main);
-    LocalBroadcastManager.getInstance(this).registerReceiver(serviceReceiver, new IntentFilter("service"));
+
+    IntentFilter filter = new IntentFilter();
+    filter.addAction("service");
+    filter.addAction("activity");
+    LocalBroadcastManager.getInstance(this).registerReceiver(serviceReceiver, filter);
 
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
-    toggleButton = findViewById(R.id.bt_start);
-    toggleButton.setOnCheckedChangeListener(this);
+    serviceLayout = findViewById(R.id.layout_service);
+
+    serviceLayout.setOnClickListener(view -> {
+      if (!isRunning) {
+        startVPN();
+      } else {
+        stopVPN();
+      }
+    });
     syncServiceState();
     EasyLog.setTextView(findViewById(R.id.log));
-  }
-
-  @Override
-  public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-    if (isChecked) {
-      startVPN();
-    } else {
-      stopVPN();
-    }
   }
 
   private void syncServiceState() {
@@ -84,6 +89,22 @@ public class MainActivity extends AppCompatActivity implements ToggleButton.OnCh
     if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
       Intent intent = new Intent(this, LocalVPNService.class);
       startService(intent);
+    }
+  }
+
+  private void setServiceLayout(boolean isServiceRunning) {
+    TextView info = serviceLayout.findViewById(R.id.tv_info);
+    TextView tip = serviceLayout.findViewById(R.id.tv_tip);
+    ImageView icon = serviceLayout.findViewById(R.id.iv_icon);
+
+    if (isServiceRunning) {
+      info.setText(R.string.service_is_running);
+      tip.setText(R.string.tap_to_stop_service);
+      icon.setImageResource(R.drawable.ic_done);
+    } else {
+      info.setText(R.string.service_is_not_running);
+      tip.setText(R.string.tap_to_start_service);
+      icon.setImageResource(R.drawable.ic_no);
     }
   }
 }
